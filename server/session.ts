7109b4048ecdd4302b7a6874bdddb0d35e82903b
Chris <csp19126@gmail.com>
2026-03-31 23:55:20 +0700

    Fixing spin crash

--- server/session.ts
+++ server/session.ts
@@ -1,6 +1,5 @@
 import session from "express-session";
 import MemoryStore from "memorystore";
-import { storage } from "./storage";
 
 const MemoryStoreSession = MemoryStore(session);
 
@@ -10,14 +9,12 @@ export function getSessionMiddleware() {
     resave: false,
     saveUninitialized: false,
     store: new MemoryStoreSession({ checkPeriod: 86400000 }),
-    // Required for Railway's proxy
     proxy: true, 
     cookie: {
       httpOnly: true,
-      // Required for Railway's HTTPS/SSL
       secure: true, 
-      sameSite: 'none',
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+      sameSite: 'none', 
+      maxAge: 7 * 24 * 60 * 60 * 1000, 
     },
   });
 }
