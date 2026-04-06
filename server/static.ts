680b1eb6bcb41235a8d6656feec5a6e9cc0c3749
Chris <csp19126@gmail.com>
2026-04-06 10:48:42 +0700

    Use RegExp for static routing to bypass Express 5 path errors

--- server/static.ts
+++ server/static.ts
@@ -1,19 +1,15 @@
 import express, { type Express } from "express";
 import path from "path";
 
-// __dirname is provided by the build banner
 export function serveStatic(app: Express) {
   // @ts-ignore
   const distPath = path.resolve(__dirname, "public");
 
   app.use(express.static(distPath));
 
-  // This "index" name inside the parentheses gives the wildcard a label
-  // which prevents the PathError crash in Express 5.
-  app.get("/:index*", (req, res, next) => {
-    if (req.path.startsWith("/api")) {
-      return next();
-    }
+  // We use a RegExp object here. 
+  // It says: "If it's not /api, send the index.html"
+  app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
